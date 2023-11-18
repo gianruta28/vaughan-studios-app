9cf58c56f70a5e8bd16462980ff307c5cf39aa4c
Gianfranco <gianjrs.98@gmail.com>
2023-11-18 17:11:16 +0100

    fix(all): minor fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
 import { FirestoreModule } from '@angular/fire/firestore';
+import { BackgroundComponent } from './modules/waitlist/background/background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FirestoreModule],
+  imports: [CommonModule, RouterOutlet, FirestoreModule, BackgroundComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
